cba86475394a1c3948bd44e2fa3ae405719b298c
Makene Neto <makenedev@gmail.com>
2026-03-31 16:31:51 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-medium">Dashboard</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eius
+      </p>
+    </div>
+  )
+}
