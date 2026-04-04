9ba00b90176c70b13733c744b3b01207efa5cfcd
Makene Neto <makeneto17@gmail.com>
2026-04-04 22:20:47 +0100

    chore: divide components

--- src/components/SideNavbar/SideNavBar.tsx
+++ src/components/SideNavbar/SideNavBar.tsx
@@ -1,11 +1,55 @@
+import {
+  ArrowLeftRight,
+  ChartNoAxesCombined,
+  CreditCard,
+  Leaf,
+  TrendingUp,
+} from "lucide-react"
+import { NavLink } from "react-router-dom"
+import NavLogo from "./NavLogo"
+
 export default function SideNavBar() {
   return (
-    <nav className="sideNavBar">
-      <a href="/">Dashboard</a>
-      <a href="/payments">Payments</a>
-      <a href="/transactions">Transactions</a>
-      <a href="/plans">Saving Plans</a>
-      <a href="/investments">Investments</a>
-    </nav>
+    <aside className="sideNavBar">
+      <NavLogo />
+
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <ChartNoAxesCombined />
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/payments"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <CreditCard />
+          Payments
+        </NavLink>
+        <NavLink
+          to="/transactions"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <ArrowLeftRight />
+          Transactions
+        </NavLink>
+        <NavLink
+          to="/plans"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <Leaf />
+          Saving Plans
+        </NavLink>
+        <NavLink
+          to="/investments"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <TrendingUp />
+          Investments
+        </NavLink>
+      </nav>
+    </aside>
   )
 }
