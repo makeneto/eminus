f97b9c9ec7555b15f17f3ea6a6ba4f1f0140d158
Makene Neto <makeneto17@gmail.com>
2026-04-04 22:18:15 +0100

    refactor: move BrowserRouter to main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Payments from "./pages/Payments"
 import Transactions from "./pages/Transactions"
 import Investments from "./pages/Investments"
 import Plans from "./pages/Plans"
+import { Route, Routes } from "react-router-dom"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Dashboard />} />
-        <Route path="/payments" element={<Payments />} />
-        <Route path="/transactions" element={<Transactions />} />
-        <Route path="/plans" element={<Plans />} />
-        <Route path="/investments" element={<Investments />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route index element={<Dashboard />} />
+      <Route path="/payments" element={<Payments />} />
+      <Route path="/transactions" element={<Transactions />} />
+      <Route path="/plans" element={<Plans />} />
+      <Route path="/investments" element={<Investments />} />
+    </Routes>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
 import "./css/index.css"
 import "./css/styles.css"
-
 import App from "./App.tsx"
 import SideNavBar from "./components/SideNavbar/SideNavBar.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <main className="layout">
-      <SideNavBar />
-
-      <main className="pages">
-        <App />
+    <BrowserRouter>
+      <main className="layout">
+        <SideNavBar />
+        <main className="pages">
+          <App />
+        </main>
       </main>
-    </main>
+    </BrowserRouter>
   </StrictMode>,
 )
