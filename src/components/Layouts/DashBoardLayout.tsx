7c4af23caba84dfb96e14cb8fdd6d470e402209a
Makene Neto <makenedev@gmail.com>
2026-04-05 17:01:31 +0100

    feat: build dashboard layout

---
+++ src/components/Layouts/DashBoardLayout.tsx
@@ -0,0 +1,10 @@
+import CardControls from "../cardSection/CardSection"
+
+export default function DashBoardLayout() {
+  return (
+    <main className="dashLayout">
+      <CardControls />
+      <main className="dashLayout__content">Content</main>
+    </main>
+  )
+}
