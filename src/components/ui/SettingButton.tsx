e9083b81fdbaf86805c2cd146494e581b9c260eb
Makene Neto <makenedev@gmail.com>
2026-04-05 17:02:04 +0100

    feat: create setting button

---
+++ src/components/ui/SettingButton.tsx
@@ -0,0 +1,9 @@
+import { EllipsisVertical } from "lucide-react"
+
+export default function SettingButton() {
+  return (
+    <button className="settingButton">
+      <EllipsisVertical />
+    </button>
+  )
+}
