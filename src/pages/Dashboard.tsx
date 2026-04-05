78ba7c1aa85544ad731e89b6c240fadcc0752cf6
Makene Neto <makeneto17@gmail.com>
2026-04-05 17:02:23 +0100

    style: stylize application

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/header/Header"
+import DashBoardLayout from "../components/Layouts/DashBoardLayout"
 
 export default function Dashboard() {
   return (
     <>
       <Header title="Dashboard" />
+      <DashBoardLayout />
     </>
   )
 }
