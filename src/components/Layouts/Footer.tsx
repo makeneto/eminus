e8465f5b10bdc49c74c2670808b33d3139f811da
Makene Neto <makeneto17@gmail.com>
2026-04-28 18:19:19 +0100

    style: set to fixed footer

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -3,10 +3,10 @@ export default function Footer() {
   const year = now.getFullYear()
 
   return (
-    <footer className="pt-7 px-5 text-sm">
+    <footer className="fixed bottom-3.5 pt-7 px-5 text-sm">
       <div className="flex gap-4">
         <span className="font-medium">Copyright © {year} Makenedev</span>
-        <div className="flex gap-4 text-gray-400">
+        <div className="flex gap-4 text-gray-400 font-light">
           <p>Privacy Policy</p>
           <p>Terms and Conditions</p>
           <p>Contact</p>
