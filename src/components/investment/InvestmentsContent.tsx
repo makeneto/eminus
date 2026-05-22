91f0cad98962a54c6c2cf0c291f0e6ca82dddde2
Makene Neto <makeneto17@gmail.com>
2026-05-22 16:36:34 +0100

    chore: split and clean components

--- src/components/investment/InvestmentsContent.tsx
+++ src/components/investment/InvestmentsContent.tsx
@@ -1,45 +1,15 @@
-import { EllipsisVertical, Plus } from "lucide-react"
 import Frame from "../ui/Frame"
 import FrameHeader from "../ui/FrameHeader"
 import SelectFilter from "../ui/SelectFilter"
-import { companies } from "@/constants/companies"
-import { Button } from "../ui/button"
+
+import { CompanyList } from "./CompanyList"
+import InvestmentsList from "./InvestmentsList"
 
 export default function InvestmentsContent() {
   return (
     <main className="invest__content">
       <section className="invest__content--companies">
-        <Frame>
-          <FrameHeader title="My Companies">
-            <button className="btn--secondary">
-              <Plus />
-              Add Company
-            </button>
-          </FrameHeader>
-
-          <ul className="invest__companies">
-            {companies.map((company) => (
-              <li className="invest__companies__item">
-                <header className="flex items-center justify-between">
-                  <div className="flex gap-2 items-center">
-                    <div className="invest__companies__item--logo">
-                      <img src={company.logo} alt={company.name} />
-                    </div>
-                    <p className="text-sm font-medium">{company.name}</p>
-                  </div>
-
-                  <Button variant="ghost" size="xs">
-                    <EllipsisVertical size={14} />
-                  </Button>
-                </header>
-
-                <h1 className="text-lg font-bold">
-                  {company.currentBalance.toLocaleString()} kz
-                </h1>
-              </li>
-            ))}
-          </ul>
-        </Frame>
+        <CompanyList />
 
         <div className="invest--feats">
           <Frame>
@@ -48,6 +18,8 @@ export default function InvestmentsContent() {
                 vs. Invested capital
               </span>
             </FrameHeader>
+
+            <InvestmentsList />
           </Frame>
 
           <Frame>
@@ -62,6 +34,7 @@ export default function InvestmentsContent() {
         <Frame>
           <FrameHeader title="Recent Activity" />
         </Frame>
+
         <Frame>
           <FrameHeader title="Top Performer" />
         </Frame>
