bd1c5fd02b3b246b13135adce35dd91bbcedd1d6
Makene Neto <makeneto17@gmail.com>
2026-05-22 16:50:25 +0100

    refactor: update companies properties

--- src/constants/companies.ts
+++ src/constants/companies.ts
@@ -2,10 +2,11 @@ interface CompaniesProps {
   id: string
   logo: string
   name: string
-  currentBalance: number
   qtd: number
   percent: number
   percentValue: number
+  investedBalance: number
+  currentBalance: number
 }
 
 export const companies: CompaniesProps[] = [
@@ -15,25 +16,28 @@ export const companies: CompaniesProps[] = [
     name: "Yango Cars",
     currentBalance: 960000,
     qtd: 2,
-    percent: 1.6,
+    percent: 5.6,
     percentValue: 80000,
+    investedBalance: 500000,
   },
   {
     id: "company-002",
-    logo: "/yango-logo.png",
-    name: "Goldera",
-    currentBalance: 128000,
-    qtd: 1,
-    percent: 1.8,
-    percentValue: 60000,
-  },
-  {
-    id: "company-003",
-    logo: "/yango-logo.png",
+    logo: "/profile.jpeg",
     name: "Marmitas",
     currentBalance: 445000,
     qtd: 1,
     percent: 2,
     percentValue: 150000,
+    investedBalance: 500000,
+  },
+  {
+    id: "company-003",
+    logo: "/eminus-dark-logo.svg",
+    name: "Goldera",
+    currentBalance: 960000,
+    qtd: 2,
+    percent: 1.6,
+    percentValue: 80000,
+    investedBalance: 500000,
   },
 ]
