757968aecdf9b0e689cd1f86d017ffc3d8fb6af0
Makene Neto <makeneto17@gmail.com>
2026-05-22 16:44:34 +0100

    chore: develop investmentList component

---
+++ src/components/investment/InvestmentsList.tsx
@@ -0,0 +1,12 @@
+import { companies } from "@/constants/companies"
+import InvestmentItem from "./InvestmentItem"
+
+export default function InvestmentsList() {
+  return (
+    <ul className="mt-2 grid gap-9">
+      {companies.map((investment) => (
+        <InvestmentItem key={investment.id} company={investment} />
+      ))}
+    </ul>
+  )
+}
