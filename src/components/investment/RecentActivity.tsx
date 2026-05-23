17a294d1fed01a6935db8f82c755bda642a21ac1
Makene Neto <makeneto17@gmail.com>
2026-05-23 18:38:35 +0100

    refactor: develop RecentActivity component

---
+++ src/components/investment/RecentActivity.tsx
@@ -0,0 +1,10 @@
+import Frame from "../ui/Frame";
+import FrameHeader from "../ui/FrameHeader";
+
+export default function RecentActivity() {
+  return (
+    <Frame>
+      <FrameHeader title="Recent Activity" />
+    </Frame>
+  )
+}
