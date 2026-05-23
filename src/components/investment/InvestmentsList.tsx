d744c620db657909492b8486f4f5bfd97d7d8a5f
Makene Neto <makeneto17@gmail.com>
2026-05-23 18:36:50 +0100

    style: reduce white space between invested companies

--- src/components/investment/InvestmentsList.tsx
+++ src/components/investment/InvestmentsList.tsx
@@ -3,7 +3,7 @@ import InvestmentItem from "./InvestmentItem"
 
 export default function InvestmentsList() {
   return (
-    <ul className="mt-2 grid gap-9">
+    <ul className="mt-2 grid gap-6">
       {companies.map((investment) => (
         <InvestmentItem key={investment.id} company={investment} />
       ))}
